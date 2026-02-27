ff205c3ae654a13c76487592f813898f8c3cf711
quinnhyx <yixuanh205@gmail.com>
2026-02-26 23:55:26 -0500

    Week 1: connect API and manually classify emails in the past 24 hours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from gmail_tool import list_messages, get_message_snippet, get_message_info
-from email_classify import classify_email, normalize_category
+from email_category import classify_email, normalize_category
 
 app = FastAPI()
 
@@ -23,41 +23,9 @@ def read_root():
 #             "category": category
 #         })
 #     return emails_with_category
+
+
 # manually classify emails
-
-# def read_emails():
-#     messages = list_messages()
-#     emails_with_category = []
-#     for m in messages:
-#         title = get_message_title(m['id'])
-#         snippet = get_message_snippet(m['id'])
-#         category = classify_email(snippet)
-#         emails_with_category.append({
-#             "title": title,
-#             "snippet": snippet,
-#             "category": category
-#         })
-#     return emails_with_category
-
-# @app.get("/emails/{category}")
-# def read_emails_by_category(category: str):
-#     messages = list_messages()
-#     normalized_query = normalize_category(category)
-#     filtered = []
-#     for m in messages:
-#         title = get_message_title(m['id'])
-#         snippet = get_message_snippet(m['id'])
-#         cat = classify_email(snippet)
-#         if normalize_category(cat) == normalized_query:
-#             filtered.append({
-#                 "title": title,
-#                 "snippet": snippet,
-#                 "category": cat
-#             })
-#     if not filtered:
-#         raise HTTPException(status_code=404, detail=f"No emails found for category '{category}'")
-#     return filtered
-
 def read_emails():
     messages = list_messages()
     emails_with_category = []
@@ -89,4 +57,6 @@ def read_emails_by_category(category: str):
                 "Description": snippet,
                 "Category": cat
             })
+    if not filtered:
+        raise HTTPException(status_code=404, detail=f"No emails found for category '{category}'")
     return filtered
